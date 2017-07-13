489e5b9f8dc25bd31a3eefa79a2751c133786267
carlosbkm <carlos.garcia.mejias@gmail.com>
2017-07-13 23:50:19 +0200

    Added initialization of variables before prediction code

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -51,6 +51,27 @@ UKF::UKF() {
 
   Hint: one or more values initialized above might be wildly off...
   */
+  
+  // initially set to false, set to true in first call of ProcessMeasurement
+  is_initialized_ = false;
+  
+  // time when the state is true, in us
+  time_us_ = 0.0;
+  
+  // state dimension
+  n_x_ = 5;
+  
+  // Augmented state dimension
+  n_aug_ = 7;
+  
+  // Sigma point spreading parameter
+  lambda_ = 3 - n_x_;
+  
+  // predicted sigma points matrix
+  Xsig_pred_ = MatrixXd(n_x_, 2 * n_aug_ + 1);
+  
+  //create vector for weights
+  weights_ = VectorXd(2 * n_aug_ + 1);
 }
 
 UKF::~UKF() {}
@@ -66,7 +87,63 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   Complete this function! Make sure you switch between lidar and radar
   measurements.
   */
-  
+  // skip predict/update if sensor type is ignored
+  if ((meas_package.sensor_type_ == MeasurementPackage::RADAR && use_radar_) ||
+      (meas_package.sensor_type_ == MeasurementPackage::LASER && use_laser_)) {
+    
+    /*****************************************************************************
+     *  Initialization
+     ****************************************************************************/
+    if (!is_initialized_) {
+      /**
+       TODO:
+       * Initialize the state x_ with the first measurement.
+       * Create the covariance matrix.
+       * Remember: you'll need to convert radar from polar to cartesian coordinates.
+       */
+      /**
+       Initialize state.
+       */
+      
+      // first measurement
+      x_ << 1, 1, 1, 1, 1;
+      
+      // init covariance matrix
+      P_ << 1, 0, 0, 0, 0,
+            0, 1, 0, 0, 0,
+            0, 0, 1, 0, 0,
+            0, 0, 0, 1, 0,
+            0, 0, 0, 0, 1;
+      
+      if (meas_package.sensor_type_ == MeasurementPackage::LASER && use_laser_) {
+        
+        x_(0) = meas_package.raw_measurements_(0);
+        x_(1) = meas_package.raw_measurements_(1);
+        
+      }
+      else if (meas_package.sensor_type_ == MeasurementPackage::RADAR && use_radar_) {
+        /**
+         Convert radar from polar to cartesian coordinates and initialize state.
+         */
+        float ro = meas_package.raw_measurements_(0);
+        float phi = meas_package.raw_measurements_(1);
+        float ro_dot = meas_package.raw_measurements_(2);
+        x_(0) = ro     * cos(phi);
+        x_(1) = ro     * sin(phi);
+        x_(2) = ro_dot * cos(phi);
+        x_(3) = ro_dot * sin(phi);
+      }
+      
+      // init timestamp
+      time_us_ = meas_package.timestamp_;
+      
+      // done initializing, no need to predict or update
+      is_initialized_ = true;
+      
+      return;
+    }
+
+  }
 }
 
 /**
