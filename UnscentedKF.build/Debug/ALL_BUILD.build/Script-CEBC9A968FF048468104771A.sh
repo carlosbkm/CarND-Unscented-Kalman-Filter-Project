0bd5e989c9a4cd2e1ec32a95f7f715cc6a04ea69
carlosbkm <carlos.garcia.mejias@gmail.com>
2017-07-25 21:49:56 +0200

    Remove the ignored directory UnscentedKF.build

--- UnscentedKF.build/Debug/ALL_BUILD.build/Script-CEBC9A968FF048468104771A.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/Carlos/Udacity/CarND/Term-2/CarND-Unscented-Kalman-Filter-Project -f /Users/Carlos/Udacity/CarND/Term-2/CarND-Unscented-Kalman-Filter-Project/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
--- UnscentedKF.build/Debug/ZERO_CHECK.build/Script-A195B260CA2645EF91AB5FEC.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/Carlos/Udacity/CarND/Term-2/CarND-Unscented-Kalman-Filter-Project -f /Users/Carlos/Udacity/CarND/Term-2/CarND-Unscented-Kalman-Filter-Project/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
